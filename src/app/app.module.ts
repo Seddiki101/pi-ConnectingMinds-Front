7e3cdd1313e1f87592fa7441c6015f3697646049
walidhsn <walid.benhassouna@esprit.tn>
2024-04-01 23:30:43 +0100

    SCRUM-19 Integrate Template into Client Angular Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
