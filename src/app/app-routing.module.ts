b5b3c12946391779964aa3b53b0bd58112360ca3
Seddiki Ossama <seddiki915@gmail.com>
2024-04-18 22:31:08 +0100

    8.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,49 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { UserLoginComponent } from './components/usersmanagement/login-user/user-login.component';
-import { RegisterUserComponent } from './components/usersmanagement/register-user/register-user.component';
-import { HomeComponent } from './components/home/home.component';
-import { ForgottenComponent } from './components/usersmanagement/forgotten/forgotten.component'; 
-import { ResetpassComponent } from './components/usersmanagement/resetpass/resetpass.component';
-import { ProfileComponent } from './components/usersmanagement/profile/profile.component';
-import { ListUserComponent } from './components/usersmanagement/list-user/list-user.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { UserLoginComponent } from "./components/usersmanagement/login-user/user-login.component";
+import { RegisterUserComponent } from "./components/usersmanagement/register-user/register-user.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { ForgottenComponent } from "./components/usersmanagement/forgotten/forgotten.component";
+import { ResetpassComponent } from "./components/usersmanagement/resetpass/resetpass.component";
+import { ProfileComponent } from "./components/usersmanagement/profile/profile.component";
+import { ListUserComponent } from "./components/usersmanagement/list-user/list-user.component";
+import { Error404Component } from "./components/error404/error404.component";
 
 const routes: Routes = [
-  {path:'login', component:UserLoginComponent},
-  {path:'register',component:RegisterUserComponent},
-  {path:'home',component:HomeComponent},
-  { path: 'forgot-password', component: ForgottenComponent },
-  { path: 'reset-password', component: ResetpassComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'listUser', component: ListUserComponent },
-  {path:'',redirectTo:'/login',pathMatch:'full'},
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full",
+    data: { title: "Sing-in" },
+  },
+  { path: "login", component: UserLoginComponent, data: { title: "Sing-in" } },
+  {
+    path: "register",
+    component: RegisterUserComponent,
+    data: { title: "Sign-up" },
+  },
+  { path: "home", component: HomeComponent, data: { title: "Home" } },
+  {
+    path: "forgot-password",
+    component: ForgottenComponent,
+    data: { title: "Forget Password" },
+  },
+  {
+    path: "reset-password",
+    component: ResetpassComponent,
+    data: { title: "Reset Password" },
+  },
+  { path: "profile", component: ProfileComponent, data: { title: "Profile" } },
+  {
+    path: "listUser",
+    component: ListUserComponent,
+    data: { title: "User List" },
+  },
+  { path: "**", component: Error404Component, data: { title: "Error" } }, // note : don't change the order of this line , this route needs to be always in the bottom
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
