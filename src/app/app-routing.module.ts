fa738c109550289ed47ed6f9a33ca80c37331610
Rania1808 <rania.brahmi@esprit.tn>
2024-05-03 16:32:26 +0100

    adding chat & user changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,65 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { UserLoginComponent } from "./components/usersmanagement/login-user/user-login.component";
+import { RegisterUserComponent } from "./components/usersmanagement/register-user/register-user.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { ForgottenComponent } from "./components/usersmanagement/forgotten/forgotten.component";
+import { ResetpassComponent } from "./components/usersmanagement/resetpass/resetpass.component";
+import { ProfileComponent } from "./components/usersmanagement/profile/profile.component";
+import { ListUserComponent } from "./components/usersmanagement/list-user/list-user.component";
+import { Error404Component } from "./components/error404/error404.component";
+import { ChatAppComponent } from "./components/chatmanagement/chat-app/chat-app.component";
 import { ListResourcesComponent } from './components/resources/list-resources/list-resources.component';
 import { ShowResourceComponent } from './components/resources/show-resource/show-resource.component';
 import { AjoutResourceComponent } from './components/resources/ajout-resource/ajout-resource.component';
 import { MyResourcesComponent } from './components/resources/my-resources/my-resources.component';
 import { ListCategoriesComponent } from './components/resources/list-categories/list-categories.component';
 
+const routes: Routes = [
+  { path: "chat", component: ChatAppComponent, data: { title: "Chat" } },
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full",
+    data: { title: "Sing-in" },
+  },
+  { path: "login", component: UserLoginComponent, data: { title: "Sing-in" } },
+  {
+    path: "register",
+    component: RegisterUserComponent,
+    data: { title: "Sign-up" },
+  },
+  { path: "home", component: HomeComponent, data: { title: "Home" } },
+  {
+    path: "forgot-password",
+    component: ForgottenComponent,
+    data: { title: "Forget Password" },
+  },
+  {
+    path: "reset-password",
+    component: ResetpassComponent,
+    data: { title: "Reset Password" },
+  },
+  { path: "profile", component: ProfileComponent, data: { title: "Profile" } },
+  {
+    path: "listUser",
+    component: ListUserComponent,
+    data: { title: "User List" },
+  },
 
-const routes: Routes = [ { path: '' , component: HomeComponent },
-{ path: 'list-categories' , component: ListCategoriesComponent },
+
+  { path: 'list-categories' , component: ListCategoriesComponent },
 { path: 'list-resources' , component: ListResourcesComponent },
 { path: 'add-resource' , component: AjoutResourceComponent},
 { path: 'my-resources/:id' , component: MyResourcesComponent},
-{ path: 'show-resource/:id' , component: ShowResourceComponent }];
+{ path: 'show-resource/:id' , component: ShowResourceComponent },
+
+
+  { path: "**", component: Error404Component, data: { title: "Error" } }, // note : don't change the order of this line , this route needs to be always in the bottom
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,61 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UserLoginComponent } from "./components/usersmanagement/login-user/user-login.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+import { RegisterUserComponent } from "./components/usersmanagement/register-user/register-user.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { DashComponent } from "./pages/dash/dash.component";
+import { ForgottenComponent } from "./components/usersmanagement/forgotten/forgotten.component";
+import { ResetpassComponent } from "./components/usersmanagement/resetpass/resetpass.component";
+import { ProfileComponent } from "./components/usersmanagement/profile/profile.component";
+import { ListUserComponent } from "./components/usersmanagement/list-user/list-user.component";
+import { Error404Component } from "./components/error404/error404.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SidenavComponent } from "./components/sidenav/sidenav.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { ChatAppComponent } from './components/chatmanagement/chat-app/chat-app.component';
+import { ChatSidebarComponent } from './components/chatmanagement/chat-sidebar/chat-sidebar.component';
+import { MainChatComponent } from "./components/chatmanagement/main-chat/main-chat.component";
+import { SendMessageModalComponent } from "./components/chatmanagement/send-message-modal/send-message-modal.component";
+import { UserDetailsComponent } from './components/chatmanagement/user-details/user-details.component';
+import { StompService } from "./service/chatmanagement/stomp-service/stomp-service.service";
 import { ListResourcesComponent } from './components/resources/list-resources/list-resources.component';
 import { ListPopularResourcesComponent } from './components/resources/list-popular-resources/list-popular-resources.component';
 import { ListCategoriesComponent } from './components/resources/list-categories/list-categories.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { AjoutResourceComponent } from './components/resources/ajout-resource/ajout-resource.component';
 import { DetailResourceComponent } from './components/resources/detail-resource/detail-resource.component';
 import { ShowResourceComponent } from './components/resources/show-resource/show-resource.component';
 import { MyResourcesComponent } from './components/resources/my-resources/my-resources.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    UserLoginComponent,
+    RegisterUserComponent,
     HomeComponent,
+    DashComponent,
+    ForgottenComponent,
+    ResetpassComponent,
+    ProfileComponent,
+    ListUserComponent,
+    Error404Component,
+    HeaderComponent,
     FooterComponent,
+    SidenavComponent,
+    NavbarComponent,
+    ChatAppComponent,
+    ChatSidebarComponent,
+    MainChatComponent,
+    SendMessageModalComponent,
+    UserDetailsComponent,
     ListResourcesComponent,
     ListPopularResourcesComponent,
     ListCategoriesComponent,
@@ -29,15 +63,9 @@ import { MyResourcesComponent } from './components/resources/my-resources/my-res
     DetailResourceComponent,
     ShowResourceComponent,
     MyResourcesComponent,
-
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [StompService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
